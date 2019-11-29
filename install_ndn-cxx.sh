2d4b83e24bf4d02ec0b24f38d0426b41f41f783e
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2019-11-29 17:20:11 +0800

    Update install_ndn-cxx.sh

--- install_ndn-cxx.sh
+++ install_ndn-cxx.sh
@@ -8,10 +8,10 @@ NDN_CXX_VERSION=0.6.6
 
 # install ndn-cxx prerequesites
 sudo apt-get install build-essential libsqlite3-dev libssl-dev curl tar -y
-sudo apt-get install doxygen graphviz python-sphinx python-pip  python3-sphinx python3-pip  -y
-sudo pip3 install sphinxcontrib-doxylink sphinxcontrib-googleanalytics
+sudo apt-get install doxygen graphviz python3-sphinx python3-pip  -y
 pip3 install --upgrade pip
 pip3 install --upgrade setuptools
+sudo pip3 install sphinxcontrib-doxylink sphinxcontrib-googleanalytics
 
 # install ndn-cxx
 cloneOrUpdate ndn-cxx https://github.com/named-data/ndn-cxx/archive/ndn-cxx-${NDN_CXX_VERSION}.tar.gz ${NDN_CXX_VERSION}
